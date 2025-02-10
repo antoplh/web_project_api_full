4e327b2bac7525fcb55cdcebff3199e8f3fb3834
antoplh <antonellaportugal00@gmail.com>
2025-02-09 19:37:22 -0500

    port new

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ mongoose
 // Global error handler
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en el puerto ${PORT}`);
